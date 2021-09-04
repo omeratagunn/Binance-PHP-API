27ca2b0ece5d4c0c44c578f005b19df974a64956
unknown <omratagn@gmail.com>
2021-09-04 17:28:22 +0200

    extended examples

--- src/examples/example.php
+++ src/examples/example.php
@@ -5,6 +5,10 @@ include_once(__DIR__.'/../../vendor/autoload.php');
 use binancephpapi\Binance;
 use binancephpapi\Config\Config;
 
-$binance = new Binance(Config::$publicKey, Config::$secretKey);
-$t = $binance->wallet()->getAllCoins();
+$binance = new Binance(Config::$publicKey, Config::$secretKey); // pass as string, then config will handle rest//
 
+$getAllCoins = $binance->wallet()->getAllCoins();
+
+$getSnapSpot = $binance->wallet()->getDailyAccountSnapShot()->spot();
+
+$getSnapSpotWithOptions = $binance->wallet()->getDailyAccountSnapShot()->spotWithOptions(3, time()- 500, time());
